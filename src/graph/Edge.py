c56399d647081014f807c1220ed583e2c66a7fa2
Saul Neri <saul.nerio.1998@gmail.com>
2022-05-18 23:49:40 -0500

    dijkstra algorithms

--- src/graph/Edge.py
+++ src/graph/Edge.py
@@ -1,10 +1,12 @@
+from random import randint
+
 class Edge:
     class_id = 0
 
-    def __init__(self, source=None, target=None, weight=0, id=None, label="", **kwargs) -> None:
+    def __init__(self, source=None, target=None, weight=None, id=None, label="", **kwargs) -> None:
         self.source = source
         self.target = target
-        self.weight = weight
+        self.weight = randint(0, 100) if not weight else weight
         self.id = Edge.class_id if not id else id
         self.label = label
         self.attr = kwargs if len(kwargs) > 0 else {}
